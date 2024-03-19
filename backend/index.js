459dc14ca045cbfc0267d03203b585df39e06e15
karthi9772 <karthi.g9772@gmail.com>
2024-03-19 14:32:26 +0530

    project Structure

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cors = require("cors");
+const puppeteer = require("puppeteer");
+const path = require("path");
+const port = 4000;
+
+const app = express();
