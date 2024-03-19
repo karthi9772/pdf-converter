da5ebea40db6f424341e5ab1f8d50d713f1ee1dd
karthi9772 <karthi.g9772@gmail.com>
2024-03-19 15:10:22 +0530

    backend Completed

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,45 @@ const path = require("path");
 const port = 4000;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const UrlToPdf = async (url, name) => {
+  // Create a browser instance
+  const browser = await puppeteer.launch();
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  // Website URL to export as pdf
+  // const website_url = 'https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/';
+
+  // Open URL in current page
+  await page.goto(url, { waitUntil: "networkidle0" });
+
+  //To reflect CSS used for screens instead of print
+  await page.emulateMediaType("screen");
+
+  // Downlaod the PDF
+  const filepath = path.join(__dirname, `${name}.pdf`);
+  const pdf = await page.pdf({
+    path: filepath,
+    margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
+    printBackground: true,
+    format: "A4",
+  });
+
+  // Close the browser instance
+  await browser.close();
+};
+
+app.post("/get_text", (req, res) => {
+  let { url, name } = req.body;
+  UrlToPdf(url, name);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
