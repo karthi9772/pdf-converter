0c4bb5f5979621d6897d6c6d97d40dd19b09c0b1
karthi9772 <karthi.g9772@gmail.com>
2024-03-19 15:22:56 +0530

    send fn frontend

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import axios from "axios";
+
 const App = () => {
-  return <div></div>;
+  const [url, setUrl] = useState("");
+  const [sent, setSent] = useState(false);
+  const [name, setName] = useState("");
+
+  const sendUrl = async (e) => {
+    e.preventDefault();
+    setSent(true);
+
+    try {
+      await axios.post("https://localhost:4000/get_text", {
+        url,
+        name,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="App">
+      {!sent ? (
+        <>
+          <h1></h1>
+        </>
+      ) : (
+        <>
+          <h1>Pdf Created</h1>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
